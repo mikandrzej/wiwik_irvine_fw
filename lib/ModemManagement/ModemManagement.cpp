f32a39f443518be9426cd1a8f81da5e2328a1543
Embedded Garage <embedded.garage.yt@gmail.com>
2024-07-22 00:36:10 +0200

    next temp

--- lib/ModemManagement/ModemManagement.cpp
+++ lib/ModemManagement/ModemManagement.cpp
@@ -4,22 +4,41 @@
 
 #define TINY_GSM_MODEM_SIM7600 // A7670's AT instruction is compatible with SIM7600
 #include <TinyGsmClient.h>
+#include <PubSubClient.h>
 
 TinyGsmSim7600 modem(SerialAT);
+PubSubClient mqtt(client);
+TinyGsmClient client(modem, 0);
 
 ModemManagement::ModemManagement(const uint32_t baudrate,
                                  const uint8_t tx_pin,
                                  const uint8_t rx_pin,
                                  const uint8_t pwr_pin,
                                  const uint8_t reset_pin,
-                                 const String pin_code)
+                                 const String pin_code,
+                                 const String apn,
+                                 const String apn_username,
+                                 const String apn_password,
+                                 const String mqtt_server,
+                                 const uint16_t mqtt_port,
+                                 const String mqtt_login,
+                                 const String mqtt_password,
+                                 const String mqtt_device_id)
 {
     this->uart_baud = baudrate;
-    this->tx_pin = tx_pin;
-    this->rx_pin = rx_pin;
-    this->pwr_pin = pwr_pin;
-    this->reset_pin = reset_pin;
-    this->pin_code = pin_code;
+    this->txPin = tx_pin;
+    this->rxPin = rx_pin;
+    this->pwrPin = pwr_pin;
+    this->resetPin = reset_pin;
+    this->pinCode = pin_code;
+    this->apn = apn;
+    this->apnUsername = apn_username;
+    this->apnPassword = apn_password;
+    this->mqttServer = mqtt_server;
+    this->mqttServerPort = mqtt_port;
+    this->mqttLogin = mqtt_login;
+    this->mqttPassword = mqtt_password;
+    this->mqttDeviceId = mqtt_device_id;
 }
 
 void ModemManagement::loop(const uint32_t uptime_ms)
@@ -27,29 +46,32 @@ void ModemManagement::loop(const uint32_t uptime_ms)
     switch (state)
     {
     case ModemManagementState::CONFIGURING:
-        SerialAT.begin(uart_baud, SERIAL_8N1, rx_pin, tx_pin);
+        SerialAT.begin(uart_baud, SERIAL_8N1, rxPin, txPin);
 
-        pinMode(pwr_pin, OUTPUT);
-        digitalWrite(pwr_pin, LOW);
+        pinMode(pwrPin, OUTPUT);
+        digitalWrite(pwrPin, LOW);
 
-        pinMode(reset_pin, OUTPUT);
-        digitalWrite(reset_pin, LOW);
+        pinMode(resetPin, OUTPUT);
+        digitalWrite(resetPin, LOW);
+
+        mqtt.setServer(mqttServer.c_str(), mqttServerPort);
+
         state = ModemManagementState::OFF;
         break;
 
     case ModemManagementState::OFF:
-        if (ModemExpectedState::SLEEP == exp_state || ModemExpectedState::ON == exp_state)
+        if (ModemExpectedPowerState::SLEEP == expPowerState || ModemExpectedPowerState::ON == expPowerState)
         {
             state = ModemManagementState::POWERING_ON;
         }
         break;
 
     case ModemManagementState::POWERING_ON:
-        digitalWrite(reset_pin, HIGH);
+        digitalWrite(resetPin, HIGH);
         vTaskDelay(pdMS_TO_TICKS(3000));
-        digitalWrite(pwr_pin, HIGH);
+        digitalWrite(pwrPin, HIGH);
         vTaskDelay(pdMS_TO_TICKS(1000));
-        digitalWrite(pwr_pin, LOW);
+        digitalWrite(pwrPin, LOW);
         vTaskDelay(pdMS_TO_TICKS(3000));
         state = ModemManagementState::BOOTING;
         break;
@@ -57,8 +79,8 @@ void ModemManagement::loop(const uint32_t uptime_ms)
     case ModemManagementState::BOOTING:
         if (modem.init())
         {
-            modem_name = modem.getModemName();
-            modem_info = modem.getModemInfo();
+            modemName = modem.getModemName();
+            modemInfo = modem.getModemInfo();
 
             state = ModemManagementState::SIM_UNLOCK;
         }
@@ -72,9 +94,11 @@ void ModemManagement::loop(const uint32_t uptime_ms)
         SimStatus sim_status = modem.getSimStatus(2000);
         if (sim_status == SIM_LOCKED)
         {
-            if (modem.simUnlock(pin_code.c_str()))
+            if (modem.simUnlock(pinCode.c_str()))
             {
-                state = ModemManagementState::NETWORK_WAIT;
+                simCcid = modem.getSimCCID();
+                simImsi = modem.getIMSI();
+                state = ModemManagementState::INIT_GPS;
             }
             else
             {
@@ -83,7 +107,9 @@ void ModemManagement::loop(const uint32_t uptime_ms)
         }
         else if (sim_status == SIM_READY)
         {
-            state = ModemManagementState::NETWORK_WAIT;
+            simCcid = modem.getSimCCID();
+            simImsi = modem.getIMSI();
+            state = ModemManagementState::INIT_GPS;
         }
         else
         {
@@ -91,32 +117,140 @@ void ModemManagement::loop(const uint32_t uptime_ms)
         }
         break;
 
+    case ModemManagementState::INIT_GPS:
+        if (ModemGpsState::INIT == gpsState)
+        {
+            ModemGpsState newGpsState = ModemGpsState::FAIL;
+
+            modem.sendAT("+CGNSSPWR=0");
+            if (modem.waitResponse(1000) == 1)
+            {
+                modem.sendAT("+CGNSSPWR=1");
+                if (modem.waitResponse(1000) == 1)
+                {
+                    if (modem.waitResponse(20000UL, "+CGNSSPWR: READY!") == 1)
+                    {
+                        modem.sendAT("+CGNSSMODE=3");
+                        if (modem.waitResponse(1000) == 1)
+                        {
+                            newGpsState = ModemGpsState::READY;
+                        }
+                    }
+                }
+            }
+
+            setGpsState(newGpsState);
+        }
+        state = ModemManagementState::NETWORK_WAIT;
+        break;
+
     case ModemManagementState::NETWORK_WAIT:
         if (modem.isNetworkConnected())
         {
-            sim_ccid = modem.getSimCCID();
-            sim_imsi = modem.getIMSI();
+            state = ModemManagementState::CONNECT_APN;
         }
-        else if (ModemExpectedState::OFF == exp_state)
+        break;
+
+    case ModemManagementState::CONNECT_APN:
+        if (!modem.isNetworkConnected())
         {
-            state = ModemManagementState::POWERING_OFF;
+            state = ModemManagementState::NETWORK_WAIT;
+        }
+        else if (modem.gprsConnect(apn.c_str(), apnUsername.c_str(), apnPassword.c_str()))
+        {
+            state = ModemManagementState::CONNECT_MQTT;
+        }
+        break;
+
+    case ModemManagementState::CONNECT_MQTT:
+        if (!modem.isGprsConnected())
+        {
+            state = ModemManagementState::CONNECT_APN;
+        }
+        else
+        {
+            bool result;
+            if (mqttLogin.length() != 0)
+            {
+                result = mqtt.connect(mqttDeviceId.c_str());
+            }
+            else
+            {
+                result = mqtt.connect(mqttDeviceId.c_str(), mqttLogin.c_str(), mqttPassword.c_str());
+            }
+
+            if (result)
+            {
+                setMqttState(ModemMqttState::READY);
+                state = ModemManagementState::ON;
+            }
+            else
+            {
+                setMqttState(ModemMqttState::FAIL);
+            }
         }
         break;
 
     case ModemManagementState::ON:
-    
+        if (expPowerState)
+        if (!modem.isNetworkConnected())
+        {
+            setMqttState(ModemMqttState::NOT_READY);
+            state = ModemManagementState::NETWORK_WAIT;
+        }
+        else if (!modem.isGprsConnected())
+        {
+            setMqttState(ModemMqttState::NOT_READY);
+            state = ModemManagementState::CONNECT_APN;
+        }
+        else if (!mqtt.connected())
+        {
+            setMqttState(ModemMqttState::NOT_READY);
+            state = ModemManagementState::CONNECT_MQTT;
+        }
         break;
 
     case ModemManagementState::POWERING_OFF:
-        digitalWrite(pwr_pin, LOW);
-        digitalWrite(reset_pin, LOW);
+        digitalWrite(pwrPin, LOW);
+        digitalWrite(resetPin, LOW);
         vTaskDelay(pdMS_TO_TICKS(3000));
         state = ModemManagementState::OFF;
+        break;
+
+    case ModemManagementState::SAFESTATE:
         break;
     }
 }
 
-void ModemManagement::requestState(const ModemExpectedState req_state)
+void ModemManagement::requestPowerState(const ModemExpectedPowerState req_state)
 {
-    exp_state = req_state;
+    expPowerState = req_state;
+}
+
+void ModemManagement::setModemGpsStateCallback(const GpsStateCallback *const callback)
+{
+    gpsStateCallback = callback;
+}
+
+void ModemManagement::setMqttStateCallback(const MqttStateCallback *const callback)
+{
+    mqttStateCallback = callback;
+}
+
+void ModemManagement::setGpsState(ModemGpsState state)
+{
+    gpsState = state;
+    if (gpsStateCallback)
+    {
+        gpsStateCallback(gpsState);
+    }
+}
+
+void ModemManagement::setMqttState(ModemMqttState state)
+{
+    mqttState = state;
+    if (mqttStateCallback)
+    {
+        mqttStateCallback(mqttState);
+    }
 }
--- lib/ModemManagement/ModemManagement.h
+++ lib/ModemManagement/ModemManagement.h
@@ -14,18 +14,39 @@ enum class ModemManagementState
     POWERING_ON,
     BOOTING,
     SIM_UNLOCK,
+    INIT_GPS,
     NETWORK_WAIT,
+    CONNECT_APN,
+    CONNECT_MQTT,
     ON,
     POWERING_OFF,
     SAFESTATE
 };
 
-enum class ModemExpectedState
+enum class ModemExpectedPowerState
 {
     OFF,
     SLEEP,
     ON
 };
+
+enum class ModemGpsState
+{
+    INIT,
+    READY,
+    FAIL
+};
+
+enum class ModemMqttState
+{
+    INIT,
+    READY,
+    NOT_READY,
+    FAIL
+};
+
+using GpsStateCallback = void(const ModemGpsState &);
+using MqttStateCallback = void(const ModemMqttState &);
 
 class ModemManagement
 {
@@ -35,27 +56,55 @@ public:
                     const uint8_t rx_pin,
                     const uint8_t pwr_pin,
                     const uint8_t reset_pin,
-                    const String pin_code);
+                    const String pin_code,
+                    const String apn,
+                    const String apn_username,
+                    const String apn_password,
+                    const String mqtt_server,
+                    const uint16_t mqtt_port,
+                    const String mqtt_login,
+                    const String mqtt_password,
+                    const String mqtt_device_id);
 
     void loop(const uint32_t uptime_ms);
-    void requestState(const ModemExpectedState req_state);
+    void requestPowerState(const ModemExpectedPowerState req_state);
+    void setModemGpsStateCallback(const GpsStateCallback *const callback);
+    void setMqttStateCallback(const MqttStateCallback *const callback);
 
 private:
-    void modemInitState();
+    void setGpsState(ModemGpsState state);
+    void setMqttState(ModemMqttState state);
 
 private:
     ModemManagementState state = ModemManagementState::CONFIGURING;
-    ModemExpectedState exp_state = ModemExpectedState::OFF;
+    ModemExpectedPowerState expPowerState = ModemExpectedPowerState::OFF;
 
     uint32_t uart_baud;
-    uint8_t tx_pin;
-    uint8_t rx_pin;
-    uint8_t pwr_pin;
-    uint8_t reset_pin;
-    String pin_code;
+    uint8_t txPin;
+    uint8_t rxPin;
+    uint8_t pwrPin;
+    uint8_t resetPin;
+    String pinCode;
 
-    String modem_name;
-    String modem_info;
-    String sim_ccid;
-    String sim_imsi;
+    String modemName;
+    String modemInfo;
+    String simCcid;
+    String simImsi;
+
+    String apn = "internet";
+    String apnUsername = "internet";
+    String apnPassword = "internet";
+
+    String mqttServer;
+    uint16_t mqttServerPort;
+    String mqttLogin;
+    String mqttPassword;
+
+    String mqttDeviceId;
+
+    ModemGpsState gpsState = ModemGpsState::INIT;
+    ModemMqttState mqttState = ModemMqttState::FAIL;
+
+    const GpsStateCallback *gpsStateCallback = nullptr;
+    const MqttStateCallback *mqttStateCallback = nullptr;
 };
