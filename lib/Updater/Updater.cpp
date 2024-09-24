02385aaf26f7a6e6b66fbfad193fe30c36122eae
Embedded Garage <embedded.garage.yt@gmail.com>
2024-09-24 23:45:49 +0200

    Split update into chunks

--- lib/Updater/Updater.cpp
+++ lib/Updater/Updater.cpp
@@ -8,136 +8,252 @@ char MODULE[] = "UPDATER";
 
 Updater updater;
 
-// bool Updater::updateTrigger(const char *server, const char *path)
-// {
-//     logger.logPrintF(LogSeverity::INFO, MODULE, "Update started with url: %s and path: %s", server, path);
-
-//     httpClient.connect(server, 80);
-
-//     // Start the OTA update
-//     int err = httpClient.get(path);
-//     if (err)
-//     {
-//         logger.logPrintF(LogSeverity::ERROR, MODULE, "HTTP GET failed with code: %d", err);
-//         httpClient.stop();
-//         return false;
-//     }
-
-//     int httpCode = httpClient.responseStatusCode();
-//     logger.logPrintF(LogSeverity::INFO, MODULE, "HTTP GET with response status code: %d", httpCode);
-//     if (httpCode != 200)
-//     {
-//         httpClient.stop();
-//         return false;
-//     }
-
-//     int length = httpClient.contentLength();
-//     logger.logPrintF(LogSeverity::DEBUG, MODULE, "HTTP Update content length %d", length);
-
-//     if (!length)
-//     {
-//         logger.logPrintF(LogSeverity::ERROR, MODULE, "HTTP Content length 0");
-//         httpClient.stop();
-//         return false;
-//     }
-
-//     if (!Update.begin(length, U_FLASH))
-//     {
-//         const char *err_str = Update.errorString();
-//         logger.logPrintF(LogSeverity::ERROR, MODULE, "HTTP Update Error %s", err_str);
-//         return false;
-//     }
-
-//     if ((Update.writeStream(httpClient) != length) || (!Update.end()))
-//     {
-//         const char *err_str = Update.errorString();
-//         logger.logPrintF(LogSeverity::ERROR, MODULE, "HTTP Update Error %s", err_str);
-//         return false;
-//     }
-
-//     logger.logPrintF(LogSeverity::INFO, MODULE, "HTTP Update Complete");
-
-//     return true;
-// }
-
 void Updater::loop()
 {
+    // todo statuses
     switch (updateState)
     {
     case UpdateState::TRIGGERED:
-        currentAddress = 0u;
-        updateState = UpdateState::IN_PROGRESS;
-        break;
-
-    case UpdateState::IN_PROGRESS:
     {
-        int remainingBytes = newFileLen - currentAddress;
-        int chunkLength = remainingBytes > sizeof(buf) ? sizeof(buf) : remainingBytes;
-
-        int bytesRead = modem.getHttpResponse(chunkLength, buf);
-        if (bytesRead <= 0)
+        modem.httpTerminate();
+        if (!modem.httpInit())
         {
-            logger.logPrintF(LogSeverity::ERROR, MODULE, "HTTP Read failed: %d", bytesRead);
-            sendUpdateStatus("HTTP Read failed");
+            updateError("HTTP init fail");
             updateState = UpdateState::IDLE;
+            break;
         }
 
-        if (bytesRead != chunkLength)
+        if (!modem.httpSetUrl(String(url)))
         {
-            logger.logPrintF(LogSeverity::ERROR, MODULE, "HTTP Read length incorrect. Expexted %d was %d", chunkLength, bytesRead);
-            sendUpdateStatus("HTTP length incorrect");
+            updateError("HTTP set URL fail");
+            updateState = UpdateState::IDLE;
+            break;
+        }
+
+        int status = modem.httpAction(HTTP_ACTION_GET);
+        if (status != 200)
+        {
+            updateError("HTTP GET chunk list fail");
+            updateState = UpdateState::IDLE;
+            break;
+        }
+        updateStatus("HTTP Get chunks OK");
+
+        updateState = UpdateState::OBTAIN_CHUNK_LIST;
+    }
+    break;
+
+    case UpdateState::OBTAIN_CHUNK_LIST:
+    {
+        bool stop = false;
+        int length = modem.getHttpResponseLength();
+        updateStatus("HTTP Get chunks length = %d", length);
+
+        if (length >= sizeof(buf))
+        {
+            updateError("Chunk list file exceed %u", sizeof(buf));
+            updateState = UpdateState::IDLE;
+            break;
+        }
+
+        int bytesRead = modem.getHttpResponse(length, buf);
+        if (bytesRead != length)
+        {
+            updateError("Chunk list HTTP Read failed");
+            updateState = UpdateState::IDLE;
+            break;
+        }
+
+        char *pch;
+        pch = strtok((char *)buf, "\t \r\n");
+
+        // Get total length
+        totalLength = atoi(pch);
+
+        pch = strtok(NULL, "\t \r\n");
+
+        uint8_t k = 0u;
+        while (pch != NULL)
+        {
+            size_t filenameLength = strlen(pch);
+            if (filenameLength == 0u)
+            {
+                logger.logPrintF(LogSeverity::WARNING, MODULE, "Empty filename");
+                continue;
+            }
+            if (filenameLength >= MAX_FILENAME_LEN)
+            {
+                updateError("Chunk filename too long: %u", filenameLength);
+                updateState = UpdateState::IDLE;
+                stop = true;
+                break;
+            }
+            strncpy(chunkFiles[k], pch, MAX_FILENAME_LEN);
+            k++;
+
+            if (k > MAX_CHUNKS)
+            {
+                updateError("Too much chunks");
+                updateState = UpdateState::IDLE;
+                stop = true;
+                break;
+            }
+
+            pch = strtok(NULL, "\t \r\n");
+        }
+        if (stop)
+        {
+            break;
+        }
+
+        if (k == 0)
+        {
+            updateError("Empty chunk list");
+            updateState = UpdateState::IDLE;
+            break;
+        }
+
+        chunkCnt = k;
+
+        for (uint8_t t = 0u; t < k; t++)
+        {
+            logger.logPrintF(LogSeverity::INFO, MODULE, "Update chunk %u: %s", t, chunkFiles[t]);
+        }
+
+        if (!Update.begin(totalLength))
+        {
+            modem.httpTerminate();
+
+            const char *err_str = Update.errorString();
+            updateError("Update error: %s", Update.errorString());
+            break;
+        }
+
+        updateStatus("Update started");
+
+        currentChunk = 0u;
+        updateState = UpdateState::GET_CHUNK;
+    }
+    break;
+
+    case UpdateState::GET_CHUNK:
+    {
+        char chunkUrl[100];
+
+        if (!buildChunkUrl(this->url, chunkFiles[currentChunk], chunkUrl))
+        {
+            updateError("Build chunk URL failed for: %s", chunkFiles[currentChunk]);
+            updateState = UpdateState::IDLE;
+            break;
+        }
+
+        logger.logPrintF(LogSeverity::DEBUG, MODULE, "chunk url: %s", chunkUrl);
+
+        if (!modem.httpSetUrl(String(chunkUrl)))
+        {
+            updateError("HTTP set URL for chunk %u fail", currentChunk);
+            updateState = UpdateState::IDLE;
+            break;
+        }
+
+        int status = modem.httpAction(HTTP_ACTION_GET);
+        if (status != 200)
+        {
+            updateError("HTTP GET for chunk %u fail", currentChunk);
+            updateState = UpdateState::IDLE;
+            break;
+        }
+
+        updateStatus("HTTP Get chunk %u OK", currentChunk);
+
+        chunkLength = modem.getHttpResponseLength();
+        if (chunkLength < 0)
+        {
+            updateError("Chunk %u read error", currentChunk);
+            updateState = UpdateState::IDLE;
+            break;
+        }
+        updateStatus("HTTP chunk %u length = %d", currentChunk, chunkLength);
+
+        currentChunkPosition = 0;
+
+        updateState = UpdateState::WRITE_CHUNK;
+    }
+    break;
+
+    case UpdateState::WRITE_CHUNK:
+    {
+        int remainingBytes = chunkLength - currentChunkPosition;
+        int stepLength = remainingBytes > sizeof(buf) ? sizeof(buf) : remainingBytes;
+
+        int bytesRead = modem.getHttpResponse(stepLength, buf);
+        if (bytesRead <= 0)
+        {
+            updateError("Chunk %u read error %d", bytesRead);
             Update.abort();
             updateState = UpdateState::IDLE;
+            break;
+        }
+
+        if (bytesRead != stepLength)
+        {
+            updateError("HTTP Read length incorrect. Expexted %d was %d", chunkLength, bytesRead);
+            Update.abort();
+            updateState = UpdateState::IDLE;
+            break;
         }
 
         size_t bytesWritten = Update.write(buf, bytesRead);
         if (bytesRead != bytesWritten)
         {
-            sendUpdateStatus("Update write failed");
-            logger.logPrintF(LogSeverity::ERROR, MODULE, "Update write length incorrect. Expexted %d was %d. Error: %s", bytesRead, bytesWritten, Update.errorString());
+            updateError("Update write failed: %s", Update.errorString());
             Update.abort();
             updateState = UpdateState::IDLE;
+            break;
         }
 
-        currentAddress += bytesWritten;
-        float progress = ((float)currentAddress * 100.0f) / (float)newFileLen;
-        logger.logPrintF(LogSeverity::INFO, MODULE, "Update progress: %.2f%%", progress);
-        sendUpdateProgress(progress);
+        currentChunkPosition += bytesWritten;
+        currentUpdatePosition += bytesWritten;
 
-        if (currentAddress >= newFileLen)
+        float progress = ((float)currentUpdatePosition * 100.0f) / (float)totalLength;
+        updateProgress(progress);
+
+        if (currentChunkPosition >= chunkLength)
         {
-            updateState = UpdateState::FINISHING;
+            if (currentUpdatePosition >= totalLength)
+            {
+                updateState = UpdateState::FINISHING;
+            }
+            else
+            {
+                currentChunk++;
+                updateState = UpdateState::GET_CHUNK;
+            }
         }
     }
     break;
 
     case UpdateState::FINISHING:
+        modem.httpTerminate();
         if (!Update.end())
         {
-            String errText = "Update error: " + String(Update.errorString());
-            sendUpdateStatus(errText.c_str());
-            const char *err_str = Update.errorString();
-            logger.logPrintF(LogSeverity::ERROR, MODULE, "Update Error %s", err_str);
+            updateError("Update end failed: %s", Update.errorString());
+            Update.abort();
+            updateState = UpdateState::IDLE;
         }
         else
         {
-            logger.logPrintF(LogSeverity::INFO, MODULE, "Update Succesfull");
-            sendUpdateStatus("Success");
+            updateStatus("Success");
+            updateState = UpdateState::BEFORE_RESET;
+            updateStatus("Module restarting...");
+            resetTimestamp = millis();
         }
-        modem.httpTerminate();
 
-        sendUpdateStatus("Module restarting...");
-        logger.logPrintF(LogSeverity::ERROR, MODULE, "Module restarting...");
-        resetTimestamp = millis();
-        updateState = UpdateState::BEFORE_RESET;
         break;
 
     case UpdateState::BEFORE_RESET:
         if (millis() - resetTimestamp > 5000)
         {
-
-            delay(5000);
-
             ESP.restart();
         }
         break;
@@ -152,75 +268,111 @@ bool Updater::updateTrigger(const char *url)
     {
         if (updateState != UpdateState::IDLE)
         {
-            sendUpdateStatus("Update in progress");
+            updateStatus("Update in progress");
             logger.logPrintF(LogSeverity::WARNING, MODULE, "Update in progress");
             break;
         }
 
-        modem.httpTerminate();
-        if (!modem.httpInit())
-        {
-            sendUpdateStatus("HTTP_init_fail");
-            logger.logPrintF(LogSeverity::ERROR, MODULE, "HTTP_init fail");
-            break;
-        }
-
-        if (!modem.httpSetUrl(String(url)))
-        {
-            modem.httpTerminate();
-
-            sendUpdateStatus("SET_URL_FAIL");
-            logger.logPrintF(LogSeverity::ERROR, MODULE, "HTTP_setUrl fail");
-            break;
-        }
-
-        int status = modem.httpAction(HTTP_ACTION_GET);
-        if (status != 200)
-        {
-            modem.httpTerminate();
-
-            sendUpdateStatus("HTTP_GET_FAIL");
-            logger.logPrintF(LogSeverity::ERROR, MODULE, "HTTP_action fail, status: %d", status);
-            break;
-        }
-        logger.logPrintF(LogSeverity::INFO, MODULE, "HTTP_action status: %d", status);
-
-        int length = modem.getHttpResponseLength();
-        logger.logPrintF(LogSeverity::INFO, MODULE, "HTTP_reponseLength: %d", length);
-
-        // Update.abort();
-        if (!Update.begin(length))
-        {
-            modem.httpTerminate();
-
-            const char *err_str = Update.errorString();
-            logger.logPrintF(LogSeverity::ERROR, MODULE, "HTTP Update Error %s", err_str);
-            String errText = "Update error: " + String(Update.errorString());
-            sendUpdateStatus(errText.c_str());
-            break;
-        }
-
-        newFileLen = length;
-        logger.logPrintF(LogSeverity::INFO, MODULE, "Update triggerred");
-        sendUpdateStatus("Update triggerred");
-
+        strncpy(this->url, url, sizeof(this->url));
         updateState = UpdateState::TRIGGERED;
 
+        result = true;
     } while (false);
 
-    return true;
+    return result;
 }
 
-void Updater::sendUpdateProgress(const float progress)
+void Updater::updateProgress(const float progress)
 {
-    sprintf(mqttTxItem.msg, R"({"progress":%.2f})", progress);
+    char val[10];
+    sprintf(val, "%.1f", progress);
+
+    logger.logPrintF(LogSeverity::INFO, MODULE, "Update progress: %s", val);
+
+    sprintf(mqttTxItem.msg, R"({"progress":%s})", val);
     sprintf(mqttTxItem.topic, "irvine/%s/update/progress", irvineConfiguration.device.deviceId);
     modemManagement.mqttPublish(mqttTxItem);
 }
 
-void Updater::sendUpdateStatus(const char *status)
+void Updater::updateStatus(const char *format, ...)
 {
+    char status[256]; // Buffer for formatted status message
+    va_list args;
+
+    // Start processing the variable arguments
+    va_start(args, format);
+
+    // Format the status string
+    vsnprintf(status, sizeof(status), format, args);
+
+    // End processing the variable arguments
+    va_end(args);
+
+    // Log the status message using the logger
+    logger.logPrintF(LogSeverity::INFO, MODULE, status);
+
+    // Use the formatted status in the MQTT message
     sprintf(mqttTxItem.msg, R"({"status":"%s"})", status);
     sprintf(mqttTxItem.topic, "irvine/%s/update/status", irvineConfiguration.device.deviceId);
-    modemManagement.mqttPublish(mqttTxItem);
+
+    // Publish the message using MQTT
+    if (!modemManagement.mqttPublish(mqttTxItem))
+    {
+        logger.logPrintF(LogSeverity::ERROR, MODULE, "MQTT Publish failed for status message");
+    }
+}
+
+void Updater::updateError(const char *format, ...)
+{
+    char status[256]; // Buffer for formatted status message
+    va_list args;
+
+    // Start processing the variable arguments
+    va_start(args, format);
+
+    // Format the status string
+    vsnprintf(status, sizeof(status), format, args);
+
+    // End processing the variable arguments
+    va_end(args);
+
+    // Log the status message using the logger
+    logger.logPrintF(LogSeverity::ERROR, MODULE, status);
+
+    // Use the formatted status in the MQTT message
+    sprintf(mqttTxItem.msg, R"({"status":"%s"})", status);
+    sprintf(mqttTxItem.topic, "irvine/%s/update/status", irvineConfiguration.device.deviceId);
+
+    // Publish the message using MQTT
+    if (!modemManagement.mqttPublish(mqttTxItem))
+    {
+        logger.logPrintF(LogSeverity::ERROR, MODULE, "MQTT Publish failed for status message");
+    }
+}
+
+bool Updater::buildChunkUrl(const char *origin, const char *chunkFilename, char *url)
+{
+    if (!origin || !chunkFilename || !url)
+    {
+        return false;
+    }
+
+    // Find the last occurrence of '/' in the origin URL
+    const char *lastSlash = strrchr(origin, '/');
+
+    if (lastSlash)
+    {
+        // Copy the origin up to the last '/'
+        size_t baseLength = lastSlash - origin + 1;
+        strncpy(url, origin, baseLength);
+        url[baseLength] = '\0'; // Null-terminate the string
+    }
+    else
+    {
+        return false;
+    }
+
+    // Concatenate the chunk filename
+    strncat(url, chunkFilename, sizeof(url) - strlen(url) - 1);
+    return true;
 }
--- lib/Updater/Updater.h
+++ lib/Updater/Updater.h
@@ -4,11 +4,16 @@
 #include <ModemManagement.h>
 #include <MqttTxItem.h>
 
+#define MAX_CHUNKS 50u
+#define MAX_FILENAME_LEN 20u
+
 enum class UpdateState
 {
     IDLE,
     TRIGGERED,
-    IN_PROGRESS,
+    OBTAIN_CHUNK_LIST,
+    GET_CHUNK,
+    WRITE_CHUNK,
     FINISHING,
     BEFORE_RESET
 };
@@ -20,14 +25,30 @@ public:
     bool updateTrigger(const char *url);
 
 private:
-    void sendUpdateProgress(const float progress);
-    void sendUpdateStatus(const char *status);
+    void updateProgress(const float progress);
+    void updateStatus(const char *format, ...);
+
+    void updateError(const char *format, ...);
+
+    bool buildChunkUrl(const char *origin, const char *chunkFilename, char *url);
+
+    UpdateState updateState = UpdateState::IDLE;
 
     uint8_t buf[4096];
-    UpdateState updateState = UpdateState::IDLE;
-    uint32_t currentAddress = 0u;
-    uint32_t newFileLen = 0u;
+    char url[128];
+
+    char chunkFiles[MAX_CHUNKS][MAX_FILENAME_LEN];
+    uint16_t currentChunk;
+    uint16_t chunkCnt;
+
+    int currentUpdatePosition = 0u;
+    int currentChunkPosition;
+
+    int chunkLength;
+    int totalLength;
+
     uint32_t resetTimestamp;
+
     MqttTxItem mqttTxItem;
 };
 
